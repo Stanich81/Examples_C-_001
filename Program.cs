36490c8c25abe77f14faff3e5f43735c4eb8dbc2
Stanislav <stanich81@gmail.com>
2023-11-18 00:30:50 +0300

    вывод числа от -Х до Х

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a*(-1);
+int min;
+int max;
+
+Console.WriteLine($"a = {a}, b = {b}");
+
+if (a>b){
+    max = a; min = b;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+else {
+    max = b; min = a;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+
+while(min < max){
+    Console.Write($"{min}, ");
+    min = min + 1; 
+}
+Console.Write($"{min}");
